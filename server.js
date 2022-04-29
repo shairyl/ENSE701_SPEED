0b5b9a577adc805a8ca7bb23e0b8949992d8cd05
shairyl <shairylsrivastava@gmail.com>
2022-04-29 12:19:29 +1200

    Setting up Heroku Deployment

--- server.js
+++ server.js
@@ -2,10 +2,22 @@
 // app.js
 
 const express = require("express");
+const config = require("config");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello world!"));
+if (config.get("env") === "prod") {
+  app.use(express.static(path.join(__dirname, "frontend/build")));
+
+  // any request that is not caught by the above routes, send back index.html
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running!");
+  });
+}
 
 const port = process.env.PORT || 8082;
 
